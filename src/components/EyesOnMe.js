ff2f74af1a0456bc0ea6c9d8c0bf52328bd47ae6
Michael <michael.d.lange88@gmail.com>
2023-05-08 20:45:47 -0700

    commit tree 275a5b63a9cc9a1f7d6d195c0f0190162490bd2a

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesonMe (){
+    function focus(event) {
+        console.log('Hey! Eyes on me!');
+      }
+
+      function blur(event) {
+        console.log('Good!');
+      }
+
+    return (
+        <div>
+            <button onFocus={blur} onBlur={focus}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesonMe;
